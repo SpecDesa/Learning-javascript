d90e75395d5dc4afc26f3fd351688814cc4fe84c
Daniel Anderson <42693718+SpecDesa@users.noreply.github.com>
2020-08-14 14:04:38 +0200

    Buttonclicks added

--- home.js
+++ home.js
@@ -1,8 +1,8 @@
 // showMessage("Changes..");
 
 // show the title
-showMessage("Title...");
-let price = 149.99;
+// showMessage("Title...");
+// let price = 149.99;
 /*
     Detail complex logic...
     Some algorithm...
@@ -15,4 +15,43 @@ let price = 149.99;
 // }
 // showMessage(person.firstname);
 
-(price < 10) ? showMessage('Less expensive than ' + price) : showMessage('More expensive than' + price);
+// (price < 10) ? showMessage('Less expensive than ' + price) : showMessage('More expensive than' + price);
+
+// for (let i = 0; i< 5; i++){
+//     console.log(i);
+// }
+
+// let myFunction = function /*can have a name here for easier debugging, but cant be used */ loggingFunction (){
+//     console.log('Here is a message');
+// }
+
+// myFunction();
+
+// changePercentOff(100);
+
+// let header = document.getElementById('message');
+
+// // header.style.color = 'red';  
+// // header.onclick = function (){header.style.color = 'yellow';}
+// header.onclick = (() => {header.style.color = 'yellow';});
+// header.style.color = 'red';  
+
+// const button = document.getElementById('see-review');
+
+// button.addEventListener('click', function(){
+//     console.log('click');
+// });
+
+const button = document.getElementById('see-review');
+button.addEventListener('click', function() {
+    const review = document.getElementById('review');
+
+    if(review.classList.contains('d-none')){
+    review.classList.remove('d-none');
+    button.textContent = 'Close review';
+    }
+    else{
+        review.classList.add('d-none');
+        button.textContent = 'See review';
+    }
+})
