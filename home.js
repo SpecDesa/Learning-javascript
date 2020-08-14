1ac998b07d4b5eccb013dc931c9e7604e98b9909
Daniel Anderson <42693718+SpecDesa@users.noreply.github.com>
2020-08-14 11:11:45 +0200

    Javascript project

---
+++ home.js
@@ -0,0 +1,18 @@
+// showMessage("Changes..");
+
+// show the title
+showMessage("Title...");
+let price = 149.99;
+/*
+    Detail complex logic...
+    Some algorithm...
+
+*/
+
+// let person = {
+//     firstname: 'John',
+//     lastname: 'Adams'
+// }
+// showMessage(person.firstname);
+
+(price < 10) ? showMessage('Less expensive than ' + price) : showMessage('More expensive than' + price);
